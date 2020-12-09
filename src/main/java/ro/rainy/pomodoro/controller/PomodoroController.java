650b57e67782ed9848964fbdef17727c312ff10c
daniel <danutdruta@gmail.com>
2020-12-09 23:19:11 +0200

    pomodoro controller

---
+++ src/main/java/ro/rainy/pomodoro/controller/PomodoroController.java
@@ -0,0 +1,32 @@
+package ro.rainy.pomodoro.controller;
+
+import ro.rainy.pomodoro.model.PomodoroModel;
+import ro.rainy.pomodoro.view.PomodoroView;
+
+/**
+ * @proiect: pomodoro
+ * @autor: daniel
+ * @data: 10/11/2020__01:38
+ */
+public class PomodoroController {
+    private PomodoroView view;
+    private PomodoroModel model;
+
+    public PomodoroController(PomodoroView view, PomodoroModel model) {
+        this.view = view;
+        this.model = model;
+        setup();
+    }
+
+    private void setup() {
+        model.whenVisibilityChange(() -> view.setVisible(model.isFrameVisible()));
+        model.whenCyclesIncrease(() -> view.setNumberOfCycle(model.getCycles()));
+        model.whenTimeTypeSwitch(() -> view.setColorOfTimeLabel(model.isRelax()));
+        model.registerClockChangeStateHandler(timeElapsed -> view.setNewCounterValue(timeElapsed));
+
+        view.whenPlayButtonClick(() -> model.whenClockStart());
+        view.whenPauseButtonClick(() -> model.whenClockPause());
+        view.whenResetButtonClick(() -> model.whenClockReset());
+
+    }
+}
