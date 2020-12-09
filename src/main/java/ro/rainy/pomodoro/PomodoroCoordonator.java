72eea54ffe43ffc52ce1714fbf5e00db548dcf5e
daniel <danutdruta@gmail.com>
2020-12-09 22:42:52 +0200

    Coordonator of app - the thing that make internal ultimate settings

---
+++ src/main/java/ro/rainy/pomodoro/PomodoroCoordonator.java
@@ -0,0 +1,23 @@
+package ro.rainy.pomodoro;
+
+import ro.rainy.pomodoro.model.PomodoroModel;
+
+/**
+ * This class should be ending of setUp & fill of all the structures in the app.
+ *
+ * @proiect: pomodoro
+ * @autor: daniel
+ * @data: 08/12/2020__23:06
+ */
+public class PomodoroCoordonator {
+
+
+    public PomodoroCoordonator(ApplicationStarter applicationStarter, PomodoroModel pomodoroModel) {
+
+        applicationStarter.whenApplicationStart(() -> {
+            pomodoroModel.loadConfigs();
+            pomodoroModel.switchTimeType();
+            pomodoroModel.setVisible(true);
+        });
+    }
+}
