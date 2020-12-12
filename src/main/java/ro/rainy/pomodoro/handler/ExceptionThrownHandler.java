ef746443288ccf6f7775703fd26540c957c5d34d
daniel <danutdruta@gmail.com>
2020-12-12 22:03:43 +0200

    exception handler

---
+++ src/main/java/ro/rainy/pomodoro/handler/ExceptionThrownHandler.java
@@ -0,0 +1,10 @@
+package ro.rainy.pomodoro.handler;
+
+/**
+ * @proiect: pomodoro
+ * @autor: daniel
+ * @data: 12/12/2020__12:42
+ */
+public interface ExceptionThrownHandler {
+    void exceptionThrown(Throwable throwable);
+}
