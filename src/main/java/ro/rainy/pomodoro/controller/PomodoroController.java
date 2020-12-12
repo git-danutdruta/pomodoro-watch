ff57f52453c61bbe096bb0a3b8b22741202539a8
daniel <danutdruta@gmail.com>
2020-12-12 22:26:55 +0200

    final setup of event direction, how are they  handled & new interactions with UI

--- src/main/java/ro/rainy/pomodoro/controller/PomodoroController.java
+++ src/main/java/ro/rainy/pomodoro/controller/PomodoroController.java
@@ -9,8 +9,8 @@ import ro.rainy.pomodoro.view.PomodoroView;
  * @data: 10/11/2020__01:38
  */
 public class PomodoroController {
-    private PomodoroView view;
-    private PomodoroModel model;
+    private final PomodoroView view;
+    private final PomodoroModel model;
 
     public PomodoroController(PomodoroView view, PomodoroModel model) {
         this.view = view;
@@ -19,14 +19,36 @@ public class PomodoroController {
     }
 
     private void setup() {
+        model.whenExceptionThrown(view::showException);
+
         model.whenVisibilityChange(() -> view.setVisible(model.isFrameVisible()));
         model.whenCyclesIncrease(() -> view.setNumberOfCycle(model.getCycles()));
         model.whenTimeTypeSwitch(() -> view.setColorOfTimeLabel(model.isRelax()));
-        model.registerClockChangeStateHandler(timeElapsed -> view.setNewCounterValue(timeElapsed));
+        model.registerClockChangeStateHandler(view::setNewCounterValue);
+        model.whenSettingsDialogVisibilityChange(() -> view.setSettingsDialogVisible(model.isSettingsDialogVisible()));
 
-        view.whenPlayButtonClick(() -> model.whenClockStart());
-        view.whenPauseButtonClick(() -> model.whenClockPause());
-        view.whenResetButtonClick(() -> model.whenClockReset());
+        model.whenSettingWorkTimeChange(() -> view.setWorkTime(model.getWorkTime()));
+        model.whenSettingPauseTimeChange(() -> view.setPauseTime(model.getPauseTime()));
+        model.whenSettingBigPauseTimeChange(() -> view.setBigPauseTime(model.getBigPauseTime()));
+        model.whenSettingCyclesTimeChange(() -> view.setCyclesOfTime(model.getCyclesOfTime()));
+
+        view.whenSettingButtonClick(() -> model.setSettingsDialogVisible(true));
+        view.whenSettingsDialogClose(model::whenSettingDialogClose);
+        view.whenPlayButtonClick(model::whenClockStart);
+        view.whenPauseButtonClick(model::whenClockPause);
+        view.whenResetButtonClick(model::whenClockReset);
+        view.whenSaveSettingsButtonClick(model::whenConfigUpdate);
+
+        view.whenWorkSliderChange(() -> view.setWorkTime(model.getWorkSliderRangeModel().getValue()));
+        view.whenPauseSliderChange(() -> view.setPauseTime(model.getPauseSliderRangeModel().getValue()));
+        view.whenBigPauseSliderChange(() -> view.setBigPauseTime(model.getBigPauseSliderRangeModel().getValue()));
+        view.whenCyclesSliderChange(() -> view.setCyclesOfTime(model.getCyclesSliderRangeModel().getValue()));
+
+        view.setWorkSliderRangeModel(model.getWorkSliderRangeModel());
+        view.setPauseSliderRangeModel(model.getPauseSliderRangeModel());
+        view.setBigPauseSliderRangeModel(model.getBigPauseSliderRangeModel());
+        view.setCyclesSliderRangeModel(model.getCyclesSliderRangeModel());
+
         view.setIconImage(model.getLogo());
 
     }
