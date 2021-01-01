fbf9d3f4452de279d0911fb581b432edd5b11940
daniel <danutdruta@gmail.com>
2021-01-01 19:11:33 +0200

    event dispatcher test class

---
+++ src/test/java/ro/rainy/pomodoro/dispatcher/EventDispatcherTest.java
@@ -0,0 +1,39 @@
+package ro.rainy.pomodoro.dispatcher;
+
+import org.jmock.Expectations;
+import org.junit.Before;
+import org.junit.Test;
+import ro.rainy.pomodoro.ContextHolder;
+import ro.rainy.pomodoro.event.EventDispatcher;
+
+/**
+ * @proiect: pomodoro-watch
+ * @autor: daniel
+ * @data: 27/12/2020__23:21
+ */
+public class EventDispatcherTest extends ContextHolder {
+
+    private TestListener testListener;
+
+    @Before
+    public void setUp() {
+        testListener = context.mock(TestListener.class, "testListener");
+    }
+
+    @Test
+    public void dispatchMethodTriggerListenerBehaviorTest() {
+        EventDispatcher<TestListener> updateListenerEventDispatcher = new EventDispatcher<>("test");
+        updateListenerEventDispatcher.addListener(testListener);
+
+        context.checking(new Expectations() {{
+            oneOf(testListener).test();
+        }});
+
+        updateListenerEventDispatcher.dispatch();
+    }
+
+
+    interface TestListener {
+        void test();
+    }
+}
