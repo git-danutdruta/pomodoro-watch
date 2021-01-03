2e09f8a36af33c00143f9b5714321bda367acb3a
daniel <danutdruta@gmail.com>
2021-01-03 17:42:18 +0200

    add pomodoro controller test

---
+++ src/test/java/ro/rainy/pomodoro/controller/PomodoroControllerTest.java
@@ -0,0 +1,27 @@
+package ro.rainy.pomodoro.controller;
+
+import org.junit.Before;
+import org.junit.Test;
+import ro.rainy.pomodoro.ContextHolder;
+import ro.rainy.pomodoro.model.PomodoroModel;
+import ro.rainy.pomodoro.view.PomodoroView;
+
+/**
+ * @proiect: pomodoro
+ * @autor: daniel
+ * @data: 03/01/2021__18:37
+ */
+public class PomodoroControllerTest extends ContextHolder {
+    private PomodoroView view;
+    private PomodoroModel model;
+
+    @Before
+    public void setUp() {
+        view = context.mock(PomodoroView.class);
+        model = context.mock(PomodoroModel.class);
+    }
+
+    @Test
+    public void test() {
+    }
+}
