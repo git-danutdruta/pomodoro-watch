deb554744c9585ab5fcfeb05aff4158ec5c9c82a
daniel <danutdruta@gmail.com>
2021-01-10 15:22:46 +0200

    extends StreamPlayer lib class

---
+++ src/main/java/ro/rainy/pomodoro/model/audio/PomodoroAudioPlayer.java
@@ -0,0 +1,11 @@
+package ro.rainy.pomodoro.model.audio;
+
+import com.goxr3plus.streamplayer.stream.StreamPlayer;
+
+/**
+ * @proiect: pomodoro
+ * @autor: daniel
+ * @data: 09/01/2021__19:11
+ */
+public class PomodoroAudioPlayer extends StreamPlayer {
+}
