10a1cfb23ac0e08195fea06c281ed0e81d80ce9c
daniel <danutdruta@gmail.com>
2021-01-20 23:11:42 +0200

    file selection change handler

---
+++ src/main/java/ro/rainy/pomodoro/handler/FileSelectionChangeHandler.java
@@ -0,0 +1,10 @@
+package ro.rainy.pomodoro.handler;
+
+/**
+ * @proiect: pomodoro
+ * @autor: daniel
+ * @data: 20/01/2021__23:08
+ */
+public interface FileSelectionChangeHandler {
+    void selectionChange();
+}
