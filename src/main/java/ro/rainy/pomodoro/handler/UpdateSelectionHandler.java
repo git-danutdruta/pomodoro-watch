60505fc40f8cc899186f76c461918467949f42fa
daniel <danutdruta@gmail.com>
2021-01-22 15:58:34 +0200

    new handler

---
+++ src/main/java/ro/rainy/pomodoro/handler/UpdateSelectionHandler.java
@@ -0,0 +1,10 @@
+package ro.rainy.pomodoro.handler;
+
+/**
+ * @proiect: pomodoro
+ * @autor: daniel
+ * @data: 17/12/2020__21:56
+ */
+public interface UpdateSelectionHandler {
+    void updateSelection(String fileName);
+}
